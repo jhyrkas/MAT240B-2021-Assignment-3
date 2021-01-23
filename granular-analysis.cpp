23ef300f21345464e5bbd1e6615bd7f40f1a842a
Jeremy Hyrkas <hyrkas@Jeremys-iMac.local>
2021-01-23 13:07:32 -0800

    spectral centroid and bug fixes

--- granular-analysis.cpp
+++ granular-analysis.cpp
@@ -35,11 +35,11 @@ void fft(CArray& x)
 }
 
 // fixed size for now
-double* hann_window() {
-    static double window[2048];
+std::vector<double> hann_window(int window_size) {
+    std::vector<double> window;
 
-    for (int i = 0; i < 2048; i++) {
-        window[i] = 0.5 * (1.0 - cos(2.0*M_PI*(i+1)/2049.0));
+    for (int i = 0; i < window_size; i++) {
+        window.push_back(0.5 * (1.0 - cos(2.0*M_PI*(i+1)/2049.0)));
     }
 
     return window;
@@ -74,7 +74,6 @@ int main(int argc, char *argv[]) {
     assert(N % 2 == 0 && N > 0);
     // print usages?
 
-    double* window = hann_window();
     int SAMPLING_RATE = 48000; // import?
     int hop_size = N / 2;
     int nfft = N*4; // more frequency "precision"
@@ -83,12 +82,13 @@ int main(int argc, char *argv[]) {
     int nframes = ceil(n / float(hop_size));
 
     CArray fft_buf(nfft);
+    std::vector<double> window = hann_window(window_size);
     int start_index = 0;
 
     // analysis loop
     for (int fr = 0; fr < nframes; fr++) {
         // should deal with size corner cases
-        int end_index = std::min(n, start_index+hop_size);
+        int end_index = std::min(n, start_index+window_size);
 
         // PART 1: easy sliding window calculations
         // let's assume reasonable audio
@@ -130,15 +130,33 @@ int main(int argc, char *argv[]) {
         }
         
         // apply hann window
-        for (int i = 0; i < window_size; i++) {
-            fft_buf[i] *= window[i];
+        if (fr == nframes - 1) {
+            for (int i = 0; i < window_size; i++) {
+                fft_buf[i] *= window[i];
+            }
+        } else {
+            // last window can be shorter than window_size, we still don't want a pop
+            std::vector<double> last_window = hann_window(end_index - start_index);
+            for (int i = 0; i < end_index - start_index; i++) {
+                fft_buf[i] *= last_window[i];
+            }
         }
 
         fft(fft_buf);
 
         // PART 3: FFT analyses
-        
-
+        double bin_step = double(SAMPLING_RATE) / nfft;
+        double frame_sc_num = 0.0;
+        double frame_sc_dem = 0.0;
+        // don't bother with negative freqs
+        for (int j = 0; j < nfft/2+1; j++) {
+            double amp = std::abs(fft_buf[j]);
+            double freq = j * bin_step;
+            frame_sc_num += (amp * freq);
+            frame_sc_dem += amp;
+        }
+        double frame_sc = frame_sc_num / frame_sc_dem;
+        std::cout << "SC: " << frame_sc << "\n";
 
         // next frame
         start_index += hop_size;
